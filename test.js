44f00bcfc47d20df548ba7dadddbcf16548b9046
alyon <alyona.kryvoruchenko@gmail.com>
2019-01-09 23:15:39 +0200

    work with test4 - create new playbook change Timeout: created it as variable

--- test.js
+++ test.js
@@ -34,9 +34,9 @@ const TIMEOUT = 10000;
     // })
 
      it("Test 1 - Log in to the site ith correct credentials", async function () {
-         await driver.wait(until.elementLocated(By.id("tenant")),5000).sendKeys("alona-qa");
+         await driver.wait(until.elementLocated(By.id("tenant")),TIMEOUT).sendKeys("alona-qa");
          await driver.findElement(By.className("button next-btn")).click();
-         let passwordElement = await driver.wait(until.elementLocated(By.css('[type=password]')), 5000);
+         let passwordElement = await driver.wait(until.elementLocated(By.css('[type=password]')), TIMEOUT);
          console.log("waited and found password element");
          await passwordElement.sendKeys(userPassword);
          console.log("enter password");
@@ -61,28 +61,28 @@ const TIMEOUT = 10000;
      //         //click the logout button
      //         await driver.findElement(By.xpath("/html/body/lg-app/lg-dashboard/header/div/div/div[1]/a[2]")).click();
      //     }
-
-         //Go with empty tenant
-         // await driver.wait(until.elementLocated(By.id("tenant")),5000).sendKeys("");
-         // await driver.wait(until.elementLocated(By.className("button next-btn")),5000).click();
-         // let wrongTenantError = await driver.wait(until.elementLocated(By.className("error-msg")), 5000).getText();
-         // console.log("error text:" + wrongTenantError);
-         // await driver.findElement(By.xpath('//label[@for="tenant"]')).getText().then(function(checkTitleEnterAccountPage){
-         //     assert (checkTitleEnterAccountPage === "Account ID", "It isn't 'Enter you account page'");
-         // });
-         //    assert(wrongTenantError === "This is a required field.", "Error of empty tenant isn't corresponded to requirement and equal to" + wrongTenantError);
-
+     //
+     //     //Go with empty tenant
+     //     await driver.wait(until.elementLocated(By.id("tenant")),TIMEOUT).sendKeys("");
+     //     await driver.wait(until.elementLocated(By.className("button next-btn")),TIMEOUT).click();
+     //     let wrongTenantError = await driver.wait(until.elementLocated(By.className("error-msg")), TIMEOUT).getText();
+     //     console.log("error text:" + wrongTenantError);
+     //     await driver.findElement(By.xpath('//label[@for="tenant"]')).getText().then(function(checkTitleEnterAccountPage){
+     //         assert (checkTitleEnterAccountPage === "Account ID", "It isn't 'Enter you account page'");
+     //     });
+     //        assert(wrongTenantError === "This is a required field.", "Error of empty tenant isn't corresponded to requirement and equal to" + wrongTenantError);
+     //
      //   //Go with not existing account name - only one case is run. I don't understand why it happens
      //     await driver.findElement(By.id("tenant")).sendKeys("test test test test test");
      //     await driver.findElement(By.className("button next-btn")).click();
-     //     let wrongTenantName = await driver.wait(until.elementLocated(By.className("error-msg")), 5000).getText();
+     //     let wrongTenantName = await driver.wait(until.elementLocated(By.className("error-msg")), TIMEOUT).getText();
      //     console.log("error text:" + wrongTenantName);
      //     await driver.findElement(By.xpath('//label[@for="tenant"]')).getText().then(function(checkTitleEnterAccountPage){
      //         assert (checkTitleEnterAccountPage === "Account ID", "It isn't 'Enter you account page'");
      //     });
      //     assert(wrongTenantName === "Wrong account ID.", "Error of empty tenant isn't corresponded to requirement and equal to" + wrongTenantName);
      // });
-     //
+
 
      // it("Test 3 - Upload Incoming contract", async function () {
      //     await driver.findElement(By.className('mat-button-wrapper')).click();
@@ -91,7 +91,7 @@ const TIMEOUT = 10000;
      //     // use action to upload the contract
      //     const actions = driver.actions({bridge: true});
      //     await actions.click(uploadContract).sendKeys('file/Incoming contrcat.docx').perform();
-     //     driver.timeout(5000);
+     //     driver.timeout(TIMEOUT);
      //     //await driver.findElement(By.css("span.pc-file-browse")).sendKeys("file/Incoming contrcat.docx");
      //
      //
@@ -100,20 +100,20 @@ const TIMEOUT = 10000;
          it ("Test 4 - Create new playbook", async function() {
              //test for playbook
              await driver.findElement(By.css("div.dashboard__nav li.ng-star-inserted")).click();
-             await driver.wait(until.elementTextContains(driver.findElement(By.className("dashboard-title")), "Policy Center"),5000);
+             await driver.wait(until.elementTextContains(driver.findElement(By.className("dashboard-title")), "Playbook Center"),TIMEOUT);
              console.log("waite Policy Center page");
-             await driver.wait(until.elementLocated(By.css("#mat-tab-label-0-1")), 5000);
+             await driver.wait(until.elementLocated(By.css("#mat-tab-label-0-1")), TIMEOUT);
              await driver.findElement(By.css("#mat-tab-label-0-1")).click();
              console.log("Click the Playbook tab");
-             await driver.wait(until.elementLocated(By.id("createNewPBButton")),5000).click();
-             console.log ("test 77888");
-             await driver.wait(until.elementLocated(By.id("playbookName")), 5000).sendKeys("New playbook - auto test");
-             console.log ("test 3654");
+             await driver.wait(until.elementLocated(By.id("createNewPBButton")), TIMEOUT).then(function (createNewPlaybook) {
+                createNewPlaybook.click();
+             });
+             console.log ("click the create playbook button");
+             await driver.wait(until.elementLocated(By.id("playbookName")), TIMEOUT).sendKeys("New playbook - auto test");
+             console.log ("enter playbook name");
              await driver.findElement(By.id("savePlaybookName")).click();
-             console.log ("test 0120");
+             console.log ("click ok button");
              await driver.findElement(By.id("contractTypeID")).getText();
-
-
          });
 
      after(async function () {
